834bbb84575b823780d50e9511f0c7809f3b27c5
Jun Zheng <junzheng@exchange.niyume.com>
2016-10-26 08:17:11 -0400

    New version that encryptes byte

--- solitaire.py
+++ solitaire.py
@@ -1,105 +1,74 @@
-import random
+import random, hashlib
 
-class solitaire_utf8():
+class SolitaireByteEncryptor():
     def __init__(self):
         pass
     
-    def init_deck(self, deck):
+    def set_deck(self, deck):
+        ''' (SolitaireDeck) -> NoneType
+        Set deck for encryption and decryption
+        '''
         self.deck = deck
         
-    def init_joker(self, joker1, joker2):
-        self.joker1 = joker1
-        self.joker2 = joker2
-    
-    def swap_down(self, deck, card):
-        ''' (list of int, int) -> NoneType
-        Swap card to one position below.
-        This method mutates the deck.
-        '''
-        card_index = deck.index(card)
-        temp = deck[card_index]
-        deck[card_index] = deck[(card_index + 1) % len(deck)]
-        deck[(card_index + 1) % len(deck)] = temp
-    
-    def move_jokers(self, deck):
-        ''' (list of int) -> NoneType
-        Move joker 1 one place down, joker 2 two place down.
-        This method mutates the deck
-        '''
-        self.swap_down(deck, self.joker1)
-        self.swap_down(deck, self.joker2)
-        self.swap_down(deck, self.joker2)
-        
-    def triple_cut(self, deck):
-        ''' (list of int) -> NoneType
-        Do a triple cut.
-        This method mutates the deck
-        '''
-        # Find index for min joker index
-        joker1_index = min(deck.index(self.joker1), deck.index(self.joker2))
-        # Find index for max joker index
-        joker2_index = max(deck.index(self.joker1), deck.index(self.joker2))
-        # Find before, middle and end of the list
-        before = deck[:joker1_index]
-        middle = deck[joker1_index:joker2_index + 1]
-        end = deck[joker2_index + 1:]
-        # Form new list
-        new_deck = end + middle + before
-        # Mutate the input list
-        for i in range(len(deck)):
-            deck[i] = new_deck[i]
-    
-    def insert_top_to_bottom(self, deck):
-        ''' (list of int) -> NoneType
-        Get the last card number, grab that amount of card from the beginning, and
-        insert them before the last card.
-        This method will mutate the list. This function does not return anything
-        REQ: Last card must <= len(deck) not fatal
-        '''
-        # Get last card number max out to be len(deck) - 1
-        last_num = min(deck[len(deck) - 1], len(deck) - 1)
-        # Find before, middle and end of the list, and form new list
-        before = deck[last_num:len(deck) - 1]
-        middle = deck[:last_num]
-        end = deck[len(deck) - 1:]
-        # Form new list
-        new_deck = before + middle + end
-        # Mutate the input list
-        for i in range(len(deck)):
-            deck[i] = new_deck[i]
-    
-    def get_card_at_top_index(self, deck):
-        ''' (list of int) -> int
-        Get first card number, and return the card at that index.
-        '''
-        # Get first card number max out to be len(deck) - 1
-        first_num = min(deck[0], len(deck) - 1)
-        return deck[first_num]
-    
     def get_next_value(self, deck):
         ''' (list of int) -> int
         Get the next value of the deck (does not need to be valid).
         This method will mutate the lsit. This function does not return anything
         '''
-        self.move_jokers(deck)
-        self.triple_cut(deck)
-        self.insert_top_to_bottom(deck)
-        return self.get_card_at_top_index(deck)
+        deck.move_jokers()
+        deck.triple_cut()
+        deck.insert_top_to_bottom()
+        return deck.get_card_at_top_index()
     
     def get_next_keystream_value(self, deck):
         ''' (list of int) -> int
         Get the next valid value of the deck (keystream value)
         This method will mutate the lsit. This function does not return anything
         '''
-        val = self.joker1
-        while val == self.joker1 or val == self.joker2:
-            val = self.get_next_value(deck)
-        return val    
-        
-    def gen_deck(self):
-        ''' () -> list of int
-        Generate a random deck for encryption
+        val = deck.joker1
+        while val == deck.joker1 or val == deck.joker2:
+            val = self.get_next_value(self.deck)
+        return val
+    
+    def encrypt(self, byte):
+        hex_msg = hex(byte)[2:].upper()
+        encrypted = ""
+        char_map = "0123456789ABCDEF"
+        for c in hex_msg:
+            key = self.get_next_keystream_value(self.deck)
+            encrypted += char_map[(key + char_map.index(c)) % 16]
+        return encrypted
+    
+    def decrypt(self, byte):
+        decrypted = ""
+        char_map = "0123456789ABCDEF"
+        for c in byte:
+            key = self.get_next_keystream_value(self.deck)
+            decrypted += char_map[(char_map.index(c) - key) % 16]
+        decrypted = int(decrypted, 16)
+        return decrypted        
+
+
+class SolitaireDeck():
+    def __init__(self, deck, joker1, joker2):
+        ''' (list of int or string) -> SolitaireDeck
+        Initialize the deck, enter a list of int or a string (password) to
+        genterate a 52 card deck.
         '''
+        if type(deck) is str:
+            self.deck = self.gen_deck_with_password(deck)
+        else:
+            self.deck = deck
+        self.deck_copy = self.deck[:]
+        self.joker1 = joker1
+        self.joker2 = joker2
+        
+    def gen_deck_with_password(self, password):
+        ''' (str) -> list of int
+        Generate a deck of card based on a string's (password) SHA result
+        '''
+        pwd_hash = hashlib.sha1(bytes(password, "utf-8")).hexdigest()
+        random.seed(pwd_hash)
         result = []
         while len(result) != 52:
             num = random.randint(1,52)
@@ -107,42 +76,120 @@ class solitaire_utf8():
                 result.append(num)
         return result
     
+    def swap_down(self, card):
+        ''' (list of int, int) -> NoneType
+        Swap card to one position below.
+        This method mutates the deck.
+        '''
+        card_index = self.deck.index(card)
+        temp = self.deck[card_index]
+        self.deck[card_index] = self.deck[(card_index + 1) % len(self.deck)]
+        self.deck[(card_index + 1) % len(self.deck)] = temp
+    
+    def move_jokers(self):
+        ''' (list of int) -> NoneType
+        Move joker 1 one place down, joker 2 two place down.
+        This method mutates the deck
+        '''
+        self.swap_down(self.joker1)
+        self.swap_down(self.joker2)
+        self.swap_down(self.joker2)
+            
+    def triple_cut(self):
+        ''' (list of int) -> NoneType
+        Do a triple cut.
+        This method mutates the deck
+        '''
+        # Find index for min joker index
+        joker1_index = min(self.deck.index(self.joker1), 
+                           self.deck.index(self.joker2))
+        # Find index for max joker index
+        joker2_index = max(self.deck.index(self.joker1),
+                           self.deck.index(self.joker2))
+        # Find before, middle and end of the list
+        before = self.deck[:joker1_index]
+        middle = self.deck[joker1_index:joker2_index + 1]
+        end = self.deck[joker2_index + 1:]
+        # Form new list
+        new_deck = end + middle + before
+        # Mutate the input list
+        for i in range(len(self.deck)):
+            self.deck[i] = new_deck[i]
+
+    def insert_top_to_bottom(self):
+        ''' (list of int) -> NoneType
+        Get the last card number, grab that amount of card from the
+        beginning, and insert them before the last card.
+        This method will mutate the list. This function does not return
+        anything
+        REQ: Last card must <= len(deck) not fatal
+        '''
+        # Get last card number max out to be len(deck) - 1
+        last_num = min(self.deck[len(self.deck) - 1], len(self.deck) - 1)
+        # Find before, middle and end of the list, and form new list
+        before = self.deck[last_num:len(self.deck) - 1]
+        middle = self.deck[:last_num]
+        end = self.deck[len(self.deck) - 1:]
+        # Form new list
+        new_deck = before + middle + end
+        # Mutate the input list
+        for i in range(len(self.deck)):
+            self.deck[i] = new_deck[i]
+    
+    def get_card_at_top_index(self):
+        ''' (list of int) -> int
+        Get first card number, and return the card at that index.
+        '''
+        # Get first card number max out to be len(deck) - 1
+        first_num = min(self.deck[0], len(self.deck) - 1)
+        return self.deck[first_num]
+    
+    def reset(self):
+        ''' () -> NoneType
+        Reset the deck
+        '''
+        self.deck = self.deck_copy[:]
+
+
+class SolitaireUTF8():
+    def __init__(self):
+        self.byte_encryptor = SolitaireByteEncryptor()
+    
+    def set_deck(self, deck):
+        ''' (SolitaireDeck) -> NoneType
+        Set deck for encryption and decryption
+        '''
+        self.deck = deck
+        self.byte_encryptor.set_deck(deck)
+    
     def encrypt(self, message):
         message = bytes(message, "utf-8")
-        hex_msg = ""
-        for b in message:
-            hex_msg += hex(b)[2:].upper()
-            
         encrypted = ""
-        char_map = "0123456789ABCDEF"
-        deck_copy = self.deck[:]
-        keychain = ""
-        for c in hex_msg:
-            key = self.get_next_keystream_value(deck_copy)
-            encrypted += char_map[(key + char_map.index(c)) % 16]
+        for b in message:
+            encrypted += self.byte_encryptor.encrypt(b)
         return encrypted
     
     def decrypt(self, message):
-        decrypted = ""
-        char_map = "0123456789ABCDEF"
-        deck_copy = self.deck[:]
-        keychain = ""
-        for c in message:
-            key = self.get_next_keystream_value(deck_copy)
-            decrypted += char_map[(char_map.index(c) - key) % 16]
-            
-        hex_list = []
-        while decrypted:
-            hex_list.append(int(decrypted[:2],16))
-            decrypted = decrypted[2:]
-        
-        hex_list = bytearray(hex_list)
-        hex_list = hex_list.decode("utf-8")
-        return hex_list
+        byte_list = []
+        while message:
+            byte_list.append(message[:2])
+            message = message[2:]
+        decrypted = []
+        for b in byte_list:
+            decrypted.append(self.byte_encryptor.decrypt(b))
+        decrypted = bytearray(decrypted)
+        decrypted = decrypted.decode("utf-8")
+        return decrypted
 
-instance = solitaire_utf8()
-deck = instance.gen_deck()
-instance.init_deck(deck)
-instance.init_joker(51,52)
-encrypted = instance.encrypt("我就是试一试，好蛋疼啊")
-instance.decrypt(encrypted)
+# Generate a deck based on password, set jokers to be 51 and 52
+deck = SolitaireDeck("this is a really good password", 51, 52)
+
+# Initialize encryptor
+encryptor = SolitaireUTF8()
+encryptor.set_deck(deck)
+
+encrypted = encryptor.encrypt("myname")
+
+deck.reset()
+
+decrypted = encryptor.decrypt(encrypted)
